cd9fc08587cf3528a3139c8c2eea0a90965da5d3
JoaoTapara <99225847+JoaoTapara@users.noreply.github.com>
2022-05-10 00:27:05 -0300

    pronto
    Login  cadastro e button navigation

--- app/src/main/java/com/example/app_cgd/Tela_Principal.java
+++ app/src/main/java/com/example/app_cgd/Tela_Principal.java
@@ -1,16 +1,41 @@
 package com.example.app_cgd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.example.app_cgd.databinding.ActivityTelaPrincipalBinding;
 
 public class Tela_Principal extends AppCompatActivity {
 
     public static String id_user;
+    private ActivityTelaPrincipalBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_principal);
+        binding = ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+
+
     }
+
+    private  void initNavigation(){
+
+        navHostFragment
+                = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+
+
+
 }
