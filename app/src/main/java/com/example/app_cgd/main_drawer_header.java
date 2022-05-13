b63a228fa1a5a2e5f65f1ddabe85920fb30d89f3
JoaoTapara <99225847+JoaoTapara@users.noreply.github.com>
2022-05-12 22:07:12 -0300

    vai

---
+++ app/src/main/java/com/example/app_cgd/main_drawer_header.java
@@ -0,0 +1,14 @@
+package com.example.app_cgd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_drawer_header extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_drawer_header);
+    }
+}
