97cb52c949487fde114b213cfa79f047d12984f4
Thais Oliveira <84998357+Never1Surrender@users.noreply.github.com>
2022-05-20 00:29:02 -0300

    tirando o retrofit

--- app/src/main/java/com/example/app_cgd/Tela_Login.java
+++ app/src/main/java/com/example/app_cgd/Tela_Login.java
@@ -31,62 +31,6 @@ public class Tela_Login extends AppCompatActivity {
 
     }
 
-    public void clique_logar(View v){
-
-        String email = edt_email.getText().toString();
-        String senha = edt_senha.getText().toString();
-
-        //monto a requisição
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Api.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        Api api = retrofit.create(Api.class);
-
-        //faz chamada no servidor
-
-        Call<Usuario> call = api.logar(email, senha);
-        call.enqueue(new Callback<Usuario>() {
-            @Override
-            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
-                //pega a resposta do servidor
-                Usuario retorno = response.body();
-
-                //verifica se encontrou
-                if(retorno.getId_gestante().equals("0")){
-
-                    Toast.makeText(getApplicationContext(), "Email ou senha Invalido", Toast.LENGTH_SHORT).show();
-                    edt_senha.setText("");
-
-                    //finish();
-
-                }else {
-
-                    String id_user = retorno.getId_gestante();
-                    Tela_Principal.id_user = id_user;
-
-
-                            Toast.makeText(Tela_Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-
-
-                    Intent tela = new Intent(getApplicationContext(), Tela_Principal.class);
-                    startActivity(tela);
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Usuario> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-//        Intent tela = new Intent(getApplicationContext(), Tela_Cadastro_Cartao.class);
-//        startActivity(tela);
-
-    }
 
 
     public void clique_cadastro(View v){
