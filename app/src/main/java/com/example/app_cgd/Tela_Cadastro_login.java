ff5457449eb0b7135dcb96f5e2f9c777149bbbe3
JoaoTapara <99225847+JoaoTapara@users.noreply.github.com>
2022-05-21 00:30:48 -0300

    Tudo Pronto
    codguinho foda aq

--- app/src/main/java/com/example/app_cgd/Tela_Cadastro_login.java
+++ app/src/main/java/com/example/app_cgd/Tela_Cadastro_login.java
@@ -1,13 +1,33 @@
 package com.example.app_cgd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.app_cgd.DTO.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+
+import org.w3c.dom.Document;
+
+import java.lang.annotation.Documented;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,18 +37,155 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Tela_Cadastro_login extends AppCompatActivity {
 
-    EditText edt_email, edt_senha, edt_nome, edt_telefone, edt_cpf;
+    private FirebaseAuth mAuth;
+
+    private Usuario u;
+
+    private EditText edt_email, edt_senha, edt_senha_conf, edt_nome, edt_telefone, edt_cpf;
+    private Button btn_cadastrar;
+
+    private String[] mensagens = {"Preencha todos os campos","Cadastro feito com sucesso","Deu Bosta"};
+
+    String usuarioID;
+
+    public Tela_Cadastro_login() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro_login);
 
-        edt_email = (EditText)findViewById(R.id.edt_email);
-        edt_senha = (EditText)findViewById(R.id.edt_senha);
-        edt_nome = (EditText)findViewById(R.id.edt_nome);
-        edt_cpf = (EditText)findViewById(R.id.edt_cpf);
-        edt_telefone = (EditText)findViewById(R.id.edt_telefone);
+        IniciarComponente();
+
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RecuperaDados(v);
+
+
+            }
+        });
+
+    }
+
+    private void RecuperaDados(View v) {
+
+
+        String nome = edt_nome.getText().toString();
+        String email = edt_email.getText().toString();
+        String senha = edt_senha.getText().toString();
+        String senha_conf = edt_senha_conf.getText().toString();
+        String cpf = edt_cpf.getText().toString();
+        String telefone = edt_telefone.getText().toString();
+
+        u = new Usuario();
+        u.setNome(edt_nome.getText().toString());
+        u.setEmail(edt_email.getText().toString());
+        u.setSenha(edt_senha.getText().toString());
+        u.setSenha_conf(edt_senha_conf.getText().toString());
+        u.setCpf(edt_cpf.getText().toString());
+        u.setTelefone_gestante(edt_telefone.getText().toString());
+
+        if(nome.isEmpty() || email.isEmpty() || senha.isEmpty() || senha_conf.isEmpty() || cpf.isEmpty() || telefone.isEmpty() ){
+
+            Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.WHITE);
+            snackbar.setTextColor(Color.BLACK);
+            snackbar.show();
+
+        } else{
+
+            CadastrarUsuario(v);
+
+        }
+    }
+
+    private  void CadastrarUsuario(View v){
+
+        mAuth.createUserWithEmailAndPassword(u.getEmail(),u.getSenha())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            u.setId(mAuth.getUid());
+                            u.setNome(edt_nome.getText().toString());
+                            u.salvar();
+
+                            //SalvarDadosUsuario();
+
+                            Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+
+                        }else {
+
+                            String erro;
+
+                            try{
+                                throw  task.getException();
+
+                            }catch (FirebaseAuthWeakPasswordException e){
+                                erro = "Senha com no mínimo 6 caracteres";
+                            }
+                            catch (FirebaseAuthUserCollisionException e){
+                                erro = "Usuario ja existente";
+                            }
+                            catch (FirebaseAuthInvalidCredentialsException e){
+                                erro = "Digite um email valido";
+                            }
+                            catch (Exception e){
+                                erro = "Erro ao cadastrar o usuario";
+                            }
+
+                            Snackbar snackbar = Snackbar.make(v,erro, Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+
+                        }
+
+                    }
+                });
+
+    }
+
+    private void SalvarDadosUsuario(){
+
+        String nome = u.getNome().toString();
+        String cpf = u.getCpf().toString();
+        String telefone = u.getTelefone_gestante().toString();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        Map<String,Object> usuario = new HashMap<>();
+        usuario.put("nome",nome);
+        usuario.put("cpf",cpf);
+        usuario.put("telefone",telefone);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+
+
+    }
+
+    private void IniciarComponente(){
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_senha = findViewById(R.id.edt_senha);
+        edt_senha_conf = findViewById(R.id.edt_senha_conf);
+        edt_nome = findViewById(R.id.edt_nome);
+        edt_cpf = findViewById(R.id.edt_cpf);
+        edt_telefone = findViewById(R.id.edt_telefone);
+
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
--- app/src/main/java/com/example/app_cgd/Tela_Login.java
+++ app/src/main/java/com/example/app_cgd/Tela_Login.java
@@ -1,108 +1,114 @@
 package com.example.app_cgd;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.app_cgd.DTO.Usuario;
-<<<<<<< Updated upstream
-=======
+
 import com.example.app_cgd.Fragments.Home_Fragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
->>>>>>> Stashed changes
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Tela_Login extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
-    EditText edt_email, edt_senha;
+    private EditText edt_email, edt_senha;
+
+    private Button btn_Logar;
+
+    private Usuario u;
+
+    private String[] mensagens = {"Email ou senha invalido"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
 
-        edt_email = (EditText)findViewById(R.id.edt_email);
-        edt_senha = (EditText)findViewById(R.id.edt_senha);
+        IniciarComponentes();
 
-    }
-
-    public void clique_logar(View v){
-
-        String email = edt_email.getText().toString();
-        String senha = edt_senha.getText().toString();
-
-        //monto a requisição
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Api.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        Api api = retrofit.create(Api.class);
-
-        //faz chamada no servidor
-
-        Call<Usuario> call = api.logar(email, senha);
-        call.enqueue(new Callback<Usuario>() {
+        btn_Logar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
-                //pega a resposta do servidor
-                Usuario retorno = response.body();
+            public void onClick(View v) {
 
-                //verifica se encontrou
-                if(retorno.getId_gestante().equals("0")){
+                ReceberDados();
+                Logar(v);
 
-                    Toast.makeText(getApplicationContext(), "Email ou senha Invalido", Toast.LENGTH_SHORT).show();
-                    edt_senha.setText("");
-
-                    //finish();
-
-                }else {
-
-<<<<<<< Updated upstream
-                    String id_user = retorno.getId_gestante();
-                    Tela_Principal.id_user = id_user;
-=======
-
-                            Toast.makeText(Tela_Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
->>>>>>> Stashed changes
-
-
-                    Intent tela = new Intent(getApplicationContext(), Tela_Principal.class);
-                    startActivity(tela);
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Usuario> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
-//        Intent tela = new Intent(getApplicationContext(), Tela_Cadastro_Cartao.class);
-//        startActivity(tela);
+    }
+
+    private void Logar(View v) {
+
+        mAuth.signInWithEmailAndPassword(u.getEmail(), u.getSenha())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+                            startActivity(new Intent(Tela_Login.this,Tela_Principal.class));
+
+                        } else {
+
+                            Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+
+                            edt_senha.setText("");
+
+                        }
+                    }
+
+
+                });
 
     }
 
+    private void ReceberDados() {
+
+        u = new Usuario();
+        u.setEmail(edt_email.getText().toString());
+        u.setSenha(edt_senha.getText().toString());
+
+    }
 
     public void clique_cadastro(View v){
         Intent tela = new Intent(this, Tela_Cadastro_login.class);
         startActivity(tela);
+    }
+
+    private  void IniciarComponentes(){
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_senha = findViewById(R.id.edt_senha);
+
+        btn_Logar = findViewById(R.id.btn_logar);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
 }
