85f21e49d3adfd536233f5e357d0c9158f320ade
0BrunoReis0 <brunorodrigues3500@gmail.com>
2022-05-22 16:46:11 -0300

    Tela_Cadastro_Contatos e Tela_Cadastro_Plano_de_Saúde
    Criei o layout da Tela_Cadastro_Contatos e Tela_Cadastro_Plano_de_Saúde

---
+++ app/src/main/java/com/example/app_cgd/Tela_Cadastro_Contatos.java
@@ -0,0 +1,14 @@
+package com.example.app_cgd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Cadastro_Contatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro_contatos);
+    }
+}
---
+++ app/src/main/java/com/example/app_cgd/Tela_Cadastro_Plano_de_Saude.java
@@ -0,0 +1,14 @@
+package com.example.app_cgd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Cadastro_Plano_de_Saude extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro_plano_de_saude);
+    }
+}
