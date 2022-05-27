40db01fb826a96d3e91af46fbf043a9f6d845ce9
0BrunoReis0 <103082027+0BrunoReis0@users.noreply.github.com>
2022-05-26 22:01:28 -0300

    Layout Telas Fragment e Tela de Agendamento
    Criei o Layout da Tela Fragment Home
    Criei  o layout da Tela de Agendamentos

---
+++ app/src/main/java/com/example/app_cgd/Tela_de_Agendamento.java
@@ -0,0 +1,14 @@
+package com.example.app_cgd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_de_Agendamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_agendamento);
+    }
+}
