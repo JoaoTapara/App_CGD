04b37fbe0e693efa04cfd6e3e4380fe5340fd9bb
JoaoTapara <99225847+JoaoTapara@users.noreply.github.com>
2022-06-26 22:52:01 -0300

    Lista

---
+++ app/src/main/java/com/example/app_cgd/SintomasAdapter.java
@@ -0,0 +1,70 @@
+package com.example.app_cgd;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.app_cgd.DTO.Sintomas;
+
+import java.util.ArrayList;
+
+public class SintomasAdapter extends RecyclerView.Adapter<SintomasAdapter.MyViewHolder> {
+
+    private ArrayList<Sintomas> lSintomas;
+    private Context context;
+
+    public ArrayList<Sintomas> getlSintomas() {
+        return lSintomas;
+    }
+
+    public SintomasAdapter(Context context, ArrayList<Sintomas> lSintomas){
+        this.lSintomas = lSintomas;
+        this.context = context;
+    }
+
+
+    //metodo para chamar a lista em uma tela
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.sintomas_lista, parent, false);
+        return new MyViewHolder(v);
+    }
+
+
+    //onde vou pegar e jogar na tela ,onde se manipula a lista
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Sintomas s = lSintomas.get(position);
+        holder.tv_humor.setText(s.getHumor());
+        holder.tv_sintomas.setText(s.getSintomas());
+        holder.tv_outros.setText(s.getOutros());
+
+    }
+
+
+    // methodo onde se add o tamanho da lista
+    @Override
+    public int getItemCount() {
+        return lSintomas.size(); // desse modo fica mais facil por estar pegando o tamanho q vc ja criou
+    }
+
+    //como se fosse o iniciar componetes
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_humor, tv_sintomas, tv_outros;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_humor = itemView.findViewById(R.id.id_humor);
+            tv_sintomas = itemView.findViewById(R.id.id_sintomas);
+            tv_outros = itemView.findViewById(R.id.id_outros);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/app_cgd/Tela_Cadastra_sintomas.java
@@ -0,0 +1,153 @@
+package com.example.app_cgd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.app_cgd.DTO.Sintomas;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Tela_Cadastra_sintomas extends AppCompatActivity {
+
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("sintomas");
+    private Button btn_cadas_sintomas;
+    private String humor, sintomas, outros;
+    private Sintomas s;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastra_sintomas);
+
+        IniciaComponentes();
+
+        Spinner_1();
+        Spinner_2();
+        Spinner_3();
+
+
+        btn_cadas_sintomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cadastrarSintomas();
+            }
+        });
+
+
+    }
+
+
+
+    private void Spinner_1(){
+
+        String[] type = new String[]{"muito bem", "meio bem", "meio ruim", "muito ruim" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                type
+        );
+
+        AutoCompleteTextView selecionado = findViewById(R.id.spinner_1);
+        selecionado.setAdapter(adapter);
+
+        selecionado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                humor = selecionado.getText().toString();
+
+                Toast.makeText(Tela_Cadastra_sintomas.this,selecionado.getText().toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    private void Spinner_2(){
+
+        String[] type = new String[]{"falta de ar", "dor na lombar", "incontinência", "insônia", "estresse" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                type
+        );
+
+        AutoCompleteTextView selecionado = findViewById(R.id.spinner_2);
+        selecionado.setAdapter(adapter);
+
+        selecionado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                sintomas = selecionado.getText().toString();
+
+                Toast.makeText(Tela_Cadastra_sintomas.this,selecionado.getText().toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    private void Spinner_3(){
+
+        String[] type = new String[]{"nem um desses", "excesso de esforço", "doença ou ferimento", "fadiga mental" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                type
+        );
+
+        AutoCompleteTextView selecionado = findViewById(R.id.spinner_3);
+        selecionado.setAdapter(adapter);
+
+        selecionado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                outros = selecionado.getText().toString();
+
+                Toast.makeText(Tela_Cadastra_sintomas.this,selecionado.getText().toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    private void IniciaComponentes(){
+
+        btn_cadas_sintomas = findViewById(R.id.btn_cadas_sintomas);
+
+    }
+
+    private void cadastrarSintomas(){
+        FirebaseUser userId = FirebaseAuth.getInstance().getCurrentUser();
+        String usuarioAtual = userId.getUid();
+        s = new Sintomas();
+        s.setHumor(humor);
+        s.setSintomas(sintomas);
+        s.setOutros(outros);
+
+        databaseReference.child(usuarioAtual).push().setValue(s);
+        Toast.makeText(this, "Sintomas Cadastrados", Toast.LENGTH_SHORT).show();
+
+        startActivity(new Intent(Tela_Cadastra_sintomas.this, Tela_Principal.class));
+        finish();
+
+
+    }
+
+}
